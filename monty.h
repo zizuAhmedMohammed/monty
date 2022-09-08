85a579e453139cf6ca1e44e1a5f7aa5f28767f16
Ziz Ahmed <azizazu0934@gmail.com>
2022-09-08 13:44:08 -0700

    Print charachter function

--- monty.h
+++ monty.h
@@ -67,6 +67,7 @@ void m_div(stack_t **stack, unsigned int line_number);
 void m_mod(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
+void m_pchar(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
