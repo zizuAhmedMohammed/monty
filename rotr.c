903d8aa31cd79f79855369fc2414f5815c4cf70d
Ziz Ahmed <azizazu0934@gmail.com>
2022-09-08 14:43:24 -0700

    Rotate to the bottom function

--- rotr.c
+++ rotr.c
@@ -1,16 +1,30 @@
 #include "monty.h"
 
 /**
- * rotr - rotates the stack to the bottom.
- * @stack: double pointer to the begining of the linked list
- * @line_number: script line number
+ * rotr - rotates the last node of a stack_t stack
+ * @stack: stack head
+ * @line_count: line count
  *
  * Return: void
  */
-void rotr(stack_t **stack, unsigned int line_number)
+void rotr(stack_t **stack, unsigned int line_count)
 {
-	(void)line_number;
+	stack_t *bottom;
+		stack_t *prev;
 
-	if (*stack)
-		*stack = (*stack)->prev;
+		(void) line_count;
+		if (!stack || !*stack || !(*stack)->next)
+			return;
+
+		bottom = *stack;
+
+		while (bottom->next)
+			bottom = bottom->next;
+
+		prev = bottom->prev;
+		bottom->next = *stack;
+		bottom->prev = NULL;
+		prev->next = NULL;
+		(*stack)->prev = bottom;
+		*stack = bottom;
 }
