9c283d5ff5596963398d0ed4f20c5925dfac6098
Ziz Ahmed <azizazu0934@gmail.com>
2022-09-08 14:23:41 -0700

    Define opcodes

--- sub.c
+++ sub.c
@@ -5,23 +5,23 @@
 #include "monty.h"
 
 /**
- * _add -  adds the first two nodes of the stack
+ * _sub -  substracts the first two nodes of the stack
  * @stack: stack given by main
  * @line_cnt: line counter
  *
  * Return: void
  */
-void _add(stack_t **stack, unsigned int line_cnt)
+void _sub(stack_t **stack, unsigned int line_cnt)
 {
 	int result;
 
 	if (!stack || !*stack || !((*stack)->next))
 	{
-
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	result = ((*stack)->next->n) + ((*stack)->n);
-	pop(stack, line_cnt); /* For top node */
+
+	result = ((*stack)->next->n) - ((*stack)->n);
+	pop(stack, line_cnt); /*For top node*/
 	(*stack)->n = result;
 }
