f9948b0a0c010b3b41630b8ad2f1e2b8ef1d383a
Ziz Ahmed <azizazu0934@gmail.com>
2022-09-08 13:46:01 -0700

    Print string

--- monty.h
+++ monty.h
@@ -68,6 +68,7 @@ void m_mod(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void m_pchar(stack_t **stack, unsigned int line_number);
+void m_pstr(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
